6ec72f613bcaa164ec9509c9da2e5fbd7218f36d
Peter Sandor <peter.sandor@tss-yonder.com>
2020-03-20 00:21:59 +0200

    fix repo url and syntax error in script

--- assets/remove-link.js
+++ assets/remove-link.js
@@ -1,8 +1,8 @@
 require([
     'gitbook',
     'jquery'
-],function(gitbook, $){
+], function(gitbook, $) {
 	gitbook.events.bind("page.change", function() {
 		$('.gitbook-link').remove();
-	}
-})
+	});
+});
